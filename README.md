b86fdff5117d194f0659d3912a02b31f290d7af0
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-12 06:18:58 -0500

    feat:proyecto nest

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# wsp-chatboot
-Twilio + node
+
---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+
+// Configura las credenciales de Twilio
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Reemplaza con tu Account SID
+const authToken = process.env.TWILIO_AUTH_TOKEN;   // Reemplaza con tu Auth Token
+const client = twilio(accountSid, authToken);
+
+const index = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para analizar los datos entrantes
+index.use(bodyParser.urlencoded({ extended: false }));
+
+// Endpoint para el webhook
+ index.post('/webhook', async (req, res) => {
+        try {
+            const response = await client.messages.create({
+                contentSid: "HX2346a7d111c0e6f1870e533d427d0a45",
+                from: "whatsapp:+14155238886",
+                to: "whatsapp:+593995068650",
+            });
+            console.log(`Message sent:  ${response.sid}`);
+        } catch (error) {
+            console.error(`Failed to send message: ${error}`);
+        }
+});
+
+// Inicia el servidor
+index.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
