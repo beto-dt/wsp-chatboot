85b261a849489b8655531e1bd1e9f7d345fd04a3
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-12 16:51:47 -0500

    feat:agrege stringify

--- index.js
+++ index.js
@@ -19,36 +19,39 @@ index.use(bodyParser.urlencoded({ extended: false }));
 // Endpoint para el webhook
  index.post('/webhook', async (req, res) => {
         const responsewsp = req.body
-        let num = 0;
+        const num = 0;
         console.log(responsewsp);
+         switch (num) {
+             case 0:
+                 if (responsewsp.Body === 'hola' || responsewsp.Body === 'Hola') {
+                     try {
+                         const response = await client.messages.create({
+                             contentSid: "HX2346a7d111c0e6f1870e533d427d0a45",
+                             from: "whatsapp:+14155238886",
+                             to: "whatsapp:+593995068650",
+                         });
+                         num = 1;
+                     } catch (error) {
+                         console.error(`Failed to send message: ${error}`);
+                     }
+                 }
 
-     if(num === 0) {
-         if (responsewsp.Body === 'hola' || responsewsp.Body === 'Hola') {
-             num = 1;
-             console.log(num);
-             try {
-                 const response = await client.messages.create({
-                     contentSid: "HX2346a7d111c0e6f1870e533d427d0a45",
-                     from: "whatsapp:+14155238886",
-                     to: "whatsapp:+593995068650",
-                 });
-             } catch (error) {
-                 console.error(`Failed to send message: ${error}`);
-             }
+                 break;
+             case 1:
+                 try {
+                     const response = await client.messages.create({
+                         contentSid: "HX788423ed6cdfde27519433fb5b2f66c7",
+                         from: "whatsapp:+14155238886",
+                         to: "whatsapp:+593995068650",
+                     });
+                     num = 2;
+                 } catch (error) {
+                     console.error(`Failed to send message: ${error}`);
+                 }
+
+                 break;
          }
-     }
 
-     if(num === 1) {
-        try {
-            const response = await client.messages.create({
-                contentSid: "HX788423ed6cdfde27519433fb5b2f66c7",
-                from: "whatsapp:+14155238886",
-                to: "whatsapp:+593995068650",
-            });
-        } catch (error) {
-            console.error(`Failed to send message: ${error}`);
-        }
-     }
 });
 // Inicia el servidor
 index.listen(PORT, () => {
