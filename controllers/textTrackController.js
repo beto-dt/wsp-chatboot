16826bd2992fef0406cbe27841efb9c15aefc1f2
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-16 14:13:33 -0500

    implementacion textTrack

--- controllers/textTrackController.js
+++ controllers/textTrackController.js
@@ -1,5 +1,33 @@
-const textTrack =  async (req, res) => {
-    console.log('TextTrack On')
+const textTrack =  ('/upload', upload.single('multimedia'), async (req, res) => {
+    try {
+        // Verificar si se ha recibido un archivo
+        if (!req.file) {
+            return res.status(400).json({ error: 'No se recibió ningún archivo' });
+        }
+
+        // Crear un objeto FormData
+        const formData = new FormData();
+        formData.append('multimedia', req.file.buffer, req.file.originalname); // Buffer como archivo
+        // Configurar los encabezados (incluyen los de form-data)
+        const headers = {
+            ...formData.getHeaders(), // Encabezados generados por FormData
+        };
+
+        // Enviar la imagen al endpoint externo
+        const externalEndpoint = 'https://general.qa-advantagemkt.mx/demo-textract/bachoco-core/app/validaTextract'; // Cambia por tu URL externa
+        const response = await axios.post(externalEndpoint, formData, { headers });
+
+        console.log('Respuesta del endpoint externo:', response.data.validacionTicket.mensajeValidacion);
+
+        // Responder al cliente que subió la imagen
+        res.status(200).json({
+            message: 'Imagen procesada y enviada al endpoint externo',
+            externalResponse: response.data,
+        });
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
+        res.status(500).json({ error: 'Error al procesar la solicitud' });
+    }
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -21,40 +21,6 @@ const upload = multer({
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 
-
-// Endpoint que recibe exclusivamente `form-data` con multimedia
-server.post('/upload', upload.single('multimedia'), async (req, res) => {
-    try {
-        // Verificar si se ha recibido un archivo
-        if (!req.file) {
-            return res.status(400).json({ error: 'No se recibió ningún archivo' });
-        }
-
-        // Crear un objeto FormData
-        const formData = new FormData();
-        formData.append('multimedia', req.file.buffer, req.file.originalname); // Buffer como archivo
-        // Configurar los encabezados (incluyen los de form-data)
-        const headers = {
-            ...formData.getHeaders(), // Encabezados generados por FormData
-        };
-
-        // Enviar la imagen al endpoint externo
-        const externalEndpoint = 'https://general.qa-advantagemkt.mx/demo-textract/bachoco-core/app/validaTextract'; // Cambia por tu URL externa
-        const response = await axios.post(externalEndpoint, formData, { headers });
-
-        console.log('Respuesta del endpoint externo:', response.data.validacionTicket.mensajeValidacion);
-
-        // Responder al cliente que subió la imagen
-        res.status(200).json({
-            message: 'Imagen procesada y enviada al endpoint externo',
-            externalResponse: response.data,
-        });
-    } catch (error) {
-        console.error('Error al procesar la solicitud:', error);
-        res.status(500).json({ error: 'Error al procesar la solicitud' });
-    }
-});
-
 hookRoutes(server);
 templateRoutes(server);
 textTrack(server);
