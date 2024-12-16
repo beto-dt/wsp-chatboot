e7eab4780fac5e1d7d57b2d9ab3b6ab75d44e2bf
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-16 13:49:05 -0500

    implementacion textTrack

--- server.js
+++ server.js
@@ -32,12 +32,15 @@ server.post('/upload', upload.single('multimedia'), async (req, res) => {
 
         // Crear un objeto FormData
         const formData = new FormData();
-        formData.append('multimedia', req.file); // Buffer como archivo
-
+        formData.append('multimedia', req.file.buffer, req.file.originalname); // Buffer como archivo
+        // Configurar los encabezados (incluyen los de form-data)
+        const headers = {
+            ...formData.getHeaders(), // Encabezados generados por FormData
+        };
 
         // Enviar la imagen al endpoint externo
         const externalEndpoint = 'https://general.qa-advantagemkt.mx/demo-textract/bachoco-core/app/validaTextract'; // Cambia por tu URL externa
-        const response = await axios.post(externalEndpoint, formData);
+        const response = await axios.post(externalEndpoint, formData, { headers });
 
         console.log('Respuesta del endpoint externo:', response.data);
 
