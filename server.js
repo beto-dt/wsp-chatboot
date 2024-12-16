0d358e6f62a07b8f93736467b99f800a2c87ff4e
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-16 14:02:50 -0500

    implementacion textTrack

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ server.post('/upload', upload.single('multimedia'), async (req, res) => {
         const externalEndpoint = 'https://general.qa-advantagemkt.mx/demo-textract/bachoco-core/app/validaTextract'; // Cambia por tu URL externa
         const response = await axios.post(externalEndpoint, formData, { headers });
 
-        console.log('Respuesta del endpoint externo:', response.data.externalResponse);
+        console.log('Respuesta del endpoint externo:', response.data['externalResponse']);
 
         // Responder al cliente que subió la imagen
         res.status(200).json({
