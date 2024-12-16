62fe727159c128e620226a3e635a469bc0f2cd87
Alberto De La Torre <albertodelatorre@Albertos-MacBook-Pro.local>
2024-12-16 14:48:39 -0500

    condiciones si es imagen o no

--- controllers/textTrackController.js
+++ controllers/textTrackController.js
@@ -4,13 +4,13 @@ const axios = require('axios');
 async function textTrack(file) {
     try {
         // Verificar si se ha recibido un archivo
-        if (!req.file) {
-            return res.status(400).json({ error: 'No se recibió ningún archivo' });
+        if (file) {
+            console.log( 'No se recibió ningún archivo');
         }
 
         // Crear un objeto FormData
         const formData = new FormData();
-        formData.append('multimedia', req.file.buffer, req.file.originalname); // Buffer como archivo
+        formData.append('multimedia', file.buffer, file.originalname); // Buffer como archivo
         // Configurar los encabezados (incluyen los de form-data)
         const headers = {
             ...formData.getHeaders(), // Encabezados generados por FormData
@@ -22,14 +22,8 @@ async function textTrack(file) {
 
         console.log('Respuesta del endpoint externo:', response.data.validacionTicket.mensajeValidacion);
 
-        // Responder al cliente que subió la imagen
-        res.status(200).json({
-            message: 'Imagen procesada y enviada al endpoint externo',
-            externalResponse: response.data,
-        });
     } catch (error) {
         console.error('Error al procesar la solicitud:', error);
-        res.status(500).json({ error: 'Error al procesar la solicitud' });
     }
 }
 
